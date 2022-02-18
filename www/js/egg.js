5e3c00c1fba1c9663f3b29ca62c3f996af6530c2
Josh Perry <joshperry@outlook.com>
2022-02-18 21:58:07 +0000

    Initial commit

---
+++ www/js/egg.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var _0x5977=["\x6C\x6F\x76\x65\x70\x6C\x61\x79\x65\x72","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x62\x6F\x64\x79","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x3C\x61\x75\x64\x69\x6F\x20\x73\x72\x63\x3D\x22\x2F\x6C\x6F\x76\x65\x2E\x6F\x67\x67\x22\x20\x69\x64\x3D\x22\x6C\x6F\x76\x65\x70\x6C\x61\x79\x65\x72\x22\x3E\x3C\x2F\x61\x75\x64\x69\x6F\x3E","\x70\x6C\x61\x79","\x6B\x65\x79\x75\x70","\x6B\x65\x79\x43\x6F\x64\x65","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72"];konami_progress=0;function play_love(){if(!document[_0x5977[1]](_0x5977[0])){document[_0x5977[4]](_0x5977[3])[0][_0x5977[2]]+=_0x5977[5];} ;document[_0x5977[1]](_0x5977[0])[_0x5977[6]]();} ;document[_0x5977[9]](_0x5977[7],function (_0xc1adx2){code=_0xc1adx2[_0x5977[8]];switch(konami_progress){case 0:;case 1:if(code==38){konami_progress++;} else {konami_progress=0;} ;break ;;case 2:;case 3:if(code==40){konami_progress++;} else {konami_progress=0;} ;break ;;case 4:;case 6:if(code==37){konami_progress++;} else {konami_progress=0;} ;break ;;case 5:;case 7:if(code==39){konami_progress++;} else {konami_progress=0;} ;break ;;case 8:if(code==66){konami_progress++;} else {konami_progress=0;} ;break ;;case 9:if(code==65){konami_progress=0;play_love();} ;break ;;} ;} ,true);
+
+}
+/*
+     FILE ARCHIVED ON 23:29:56 Apr 30, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:52:55 Feb 18, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 80.792
+  exclusion.robots: 0.18
+  exclusion.robots.policy: 0.172
+  RedisCDXSource: 0.588
+  esindex: 0.008
+  LoadShardBlock: 57.252 (3)
+  PetaboxLoader3.datanode: 88.049 (4)
+  CDXLines.iter: 14.237 (3)
+  load_resource: 84.49
+  PetaboxLoader3.resolve: 41.068
+*/
